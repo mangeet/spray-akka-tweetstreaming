fdb3dcc1b0f8048c2b39f6054e0f1a7699feeebc
mangeetsingh_eden <mangeetsingh_eden@apple.com>
2015-11-02 11:17:22 -0800

    adding scripts and running jar

--- bin/start-streaming.sh
+++ bin/start-streaming.sh
@@ -5,4 +5,4 @@ cd ..
 mvn package
 
 echo "Start streaming tweets for query '$1'"
-java -jar target/spray-akka-tweetstreaming-0.0.1-SNAPSHOT.jar $1
+java -jar target/spray-akka-tweetstreaming-0.0.1-SNAPSHOT.jar "$1"
